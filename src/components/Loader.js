0793f85ae0390f8dd78901ec19c36a3bdbda4dd3
rahulmskh <rahulpass05@gmail.com>
2022-09-14 23:37:53 +0530

    1st commit

---
+++ src/components/Loader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => (<span className="loader" />);
+export default Loader;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '9b61612d97mshcec7a4a5df9fe98p11a11ejsnb9c3b6e8fafd',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
